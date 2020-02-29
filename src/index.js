e07b19b376d9bbe41dd6d0754519d2ec31675292
scotch115 <jordangamache115@gmail.com>
2020-02-29 14:15:08 -0500

    Adding a conditional statement to determine what database to pull,

--- src/index.js
+++ src/index.js
@@ -64,16 +64,16 @@ class App extends Component {
   }
 
 	componentDidMount() {
-		const itemsRef = firebase.database().ref(`entries/${firebase.auth().currentUser.displayName}`);
+		//	const itemsRef = firebase.database().ref(`entries/${firebase.auth().currentUser.displayName}`);
 		// TODO: Add user-specific database folders based on login
-		// firebase.auth().onAuthStateChanged(function(user){
-		// 	if (user) {
-		// 		itemsRef = firebase.database().ref(`entries/${firebase.auth().currentUser.displayName}`);
-		// 	}
-		// 	else {
-		// 		itemsRef = firebase.database().ref('entries/default');
-		// 	}
-		// });
+		 firebase.auth().onAuthStateChanged(function(user){
+		 	if (user) {
+		 		itemsRef = firebase.database().ref(`entries/${firebase.auth().currentUser.displayName}`);
+		 	}
+		 	else {
+		 		itemsRef = firebase.database().ref('entries/');
+		 	}
+		 });
 		itemsRef.on('value', (snapshot) => {
 			let entries = snapshot.val();
 			let newState = [];
